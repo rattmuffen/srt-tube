581cd742a7add0d73f02b5791f27cfb2398d33c6
Magnus <rattmuffen@gmail.com>
2014-11-22 11:58:26 +0100

    Playback states and options.
    Added playback states connected to player events and option to display
    timer. Also added some rVas to the Youtube player object (modest
    branding & autohide).

--- public/js/main.js
+++ public/js/main.js
@@ -2,31 +2,61 @@ $(document).ready(function() {
 	$.material.init();
 });
 
-
 var app = angular.module('srt-tube', ['youtube-embed']);
+
 app.controller('SubCtrl', function ($scope, $interval, $http) {
+	$scope.PlaybackState = {
+		PLAYING : 0,
+		PAUSED : 1,
+		STOPPED : 2
+	}
+	$scope.state = $scope.PlaybackState.STOPPED;
+	
+	$scope.displayTimer = false;
 	$scope.time = 0;
 	$scope.lastTime = 0;
 	
-	$scope.isPlaying = false;
-	
 	$scope.player = null;
+	$scope.playerOptions = {
+		autohide: 1,
+		modestbranding: 1
+	};	
 	$scope.videoId;
 	$scope.subs = [];
 	
+	// Timer.
 	$interval(function() {
 		var now = new Date().getTime();
-		if ($scope.isPlaying) {
-			if (now > $scope.lastTime) {
-				$scope.time += (now - $scope.lastTime);
+		switch($scope.state) {
+			case $scope.PlaybackState.PLAYING:
+				if (now > $scope.lastTime) {
+					$scope.time += (now - $scope.lastTime);
+					$scope.lastTime = now;
+				}
+				break;
+			case $scope.PlaybackState.STOPPED:
 				$scope.lastTime = now;
-			}
-		} else {
-			$scope.lastTime = now
-			$scope.time = 0;
+				$scope.time = 0;
+				break;
+			case $scope.PlaybackState.PAUSED:
+				$scope.lastTime = now;
+				break;				
 		}
     }, 1);
 	
+	// Player events,
+	$scope.$on('youtube.player.paused', function ($event, player) {
+		$scope.state = $scope.PlaybackState.PAUSED;
+	});
+	
+	$scope.$on('youtube.player.ended', function ($event, player) {
+		$scope.state = $scope.PlaybackState.STOPPED;
+	});	
+	
+	$scope.$on('youtube.player.playing', function ($event, player) {
+		$scope.state = $scope.PlaybackState.PLAYING;
+	});
+		
 	$scope.uploadFile = function(files) {
 		var fd = new FormData();
 		//Take the first selected file
@@ -49,19 +79,36 @@ app.controller('SubCtrl', function ($scope, $interval, $http) {
 	}
 	
 	$scope.togglePlay = function () {
-		$scope.isPlaying = !$scope.isPlaying;
-	  	$scope.time = 0;
-
-	  if ($scope.isPlaying) {
-		if ($scope.player)
-		  $scope.player.playVideo();
-	  } else {
-		  if ($scope.player)
-			  $scope.player.stopVideo();
-	  }
+		switch($scope.state) {
+			case $scope.PlaybackState.PLAYING:
+				// Pause video.
+				$scope.player.pauseVideo();
+				$scope.state = $scope.PlaybackState.PAUSED;
+				break;
+			case $scope.PlaybackState.PAUSED:
+			case $scope.PlaybackState.STOPPED:
+				if ($scope.player) {
+					// Play video.
+					$scope.player.playVideo();
+					$scope.state = $scope.PlaybackState.PLAYING;
+				}
+				break;
+		}
 	}
 	
 	$scope.isDisplay = function (line) {
-	  return $scope.time >= line.startTime && $scope.time <= line.endTime;
+		if ($scope.state == $scope.PlaybackState.STOPPED) return false;
+		return $scope.time >= line.startTime && $scope.time <= line.endTime;
+	}
+	
+	$scope.getPlaybackIcon = function () {
+		switch($scope.state) {
+			case $scope.PlaybackState.PLAYING:
+				return 'mdi-av-pause';
+			case $scope.PlaybackState.PAUSED:
+			case $scope.PlaybackState.STOPPED:
+				return 'mdi-av-play-arrow';
+		}
+
 	}
 });
