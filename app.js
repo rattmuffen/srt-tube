edefc39bf7ca7a7666728d86f7c78eeaafd7a10e
hagward <anders.hagward@gmail.com>
2014-12-05 14:01:33 +0100

    Fixed some syntactic inconsistencies
    - Now using four spaces for indentation instead of mixed spaces and
    tabs.
    - Now only using ' ' for strings instead of mixed ' ' and " ".
    - Now always having a space after the function keyword and () instead of
    mixed.
    - Added some missing semicolons.
    - Changed all comparison operators from == to ===.

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /* jslint node: true */
-"use strict";
+'use strict';
 
 var express = require('express');
 var morgan = require('morgan');
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var formidable = require('formidable');
 var fs = require('fs');
-var parser = require("subtitles-parser");
+var parser = require('subtitles-parser');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
@@ -15,18 +15,18 @@ app.use(morgan('dev'));
 
 var router = express.Router();
 
-router.post('/upload', function(req, res, next) {
-	var form = new formidable.IncomingForm();
-	form.parse(req, function(err, fields, files) {
-		var f = files.file.path;
-		var data = fs.readFileSync(f, "utf-8");
-		var subs = parser.fromSrt(data, true);
-		res.send(subs);
-	});		
+router.post('/upload', function (req, res, next) {
+    var form = new formidable.IncomingForm();
+    form.parse(req, function (err, fields, files) {
+        var f = files.file.path;
+        var data = fs.readFileSync(f, 'utf-8');
+        var subs = parser.fromSrt(data, true);
+        res.send(subs);
+    });        
 });
 
 app.use('/sub', router);
 
-var server = app.listen(7845, function() {
+var server = app.listen(7845, function () {
     console.log('Listening on port %d', server.address().port);
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,123 +1,132 @@
 /* jslint node: true */
-"use strict";
+'use strict';
 
-$(document).ready(function() {
-	$.material.init();
+$(document).ready(function () {
+    $.material.init();
 });
 
 var app = angular.module('srt-tube', ['youtube-embed','ngSanitize']);
 
 app.controller('SubCtrl', function ($scope, $interval, $http) {
-	$scope.PlaybackState = {
-		PLAYING : 0,
-		PAUSED : 1,
-		STOPPED : 2
-	};
-	$scope.state = $scope.PlaybackState.STOPPED;
+    $scope.PlaybackState = {
+        PLAYING : 0,
+        PAUSED : 1,
+        STOPPED : 2
+    };
+    $scope.state = $scope.PlaybackState.STOPPED;
 
-	$scope.displayTimer = false;
-	$scope.time = 0;
-	$scope.lastTime = 0;
+    $scope.displayTimer = false;
+    $scope.time = 0;
+    $scope.lastTime = 0;
 
-	$scope.player = null;
-	$scope.playerOptions = {
-		autohide: 1,
-		modestbranding: 1
-	};
-    $scope.videoId;
-	$scope.subs = [];
+    $scope.player = null; // TODO: This never gets assigned?
+    $scope.playerOptions = {
+        autohide: 1,
+        modestbranding: 1
+    };
+    $scope.videoId = null;
+    $scope.subs = [];
 
     $scope.hasAlreadyThrownErrorMessageInUsersFace = false;
 
-	$scope.fontSize = 24;
-	
-	$scope.fontFamily = 'inherit';
-	$scope.fontFamilies = ['inherit','sans-serif','serif','monospace','fantasy','cursive']
-	
-	$scope.fontColor = 'white';
-	$scope.fontColors = ['white','yellow','red','green','blue','black']
-    
+    $scope.fontSize = 24;
+
+    $scope.fontFamily = 'inherit';
+    $scope.fontFamilies = ['inherit','sans-serif','serif','monospace','fantasy','cursive'];
+
+    $scope.fontColor = 'white';
+    $scope.fontColors = ['white','yellow','red','green','blue','black'];
+
     $scope.timeOffset = 0;
     $scope.currentSrt = '';
 
-	// Timer.
-	$interval(function() {
-		var now = new Date().getTime();
-		switch($scope.state) {
-			case $scope.PlaybackState.PLAYING:
-				if (now > $scope.lastTime) {
-					$scope.time += (now - $scope.lastTime);
-					$scope.lastTime = now;
-				}
-				break;
-			case $scope.PlaybackState.STOPPED:
-				$scope.lastTime = now;
-				$scope.time = 0;
-				break;
-			case $scope.PlaybackState.PAUSED:
-				$scope.lastTime = now;
-				break;
-		}
+    // Here we have our subtitle sync timer, which is fired every millisecond.
+    // However, because some update interval inaccuracies may occur we have to
+    // also manually check that we are in sync.
+    $interval(function () {
+        var now = Date.now();
+        switch ($scope.state) {
+            case $scope.PlaybackState.PLAYING:
+                if (now > $scope.lastTime) {
+                    $scope.time += (now - $scope.lastTime);
+                    $scope.lastTime = now;
+                }
+                break;
+            case $scope.PlaybackState.STOPPED:
+                $scope.lastTime = now;
+                $scope.time = 0;
+                break;
+            case $scope.PlaybackState.PAUSED:
+                $scope.lastTime = now;
+                break;
+        }
     }, 1);
 
-	// Player events,
-	$scope.$on('youtube.player.paused', function ($event, player) {
-		$scope.state = $scope.PlaybackState.PAUSED;
+    // YouTube player events.
+
+    $scope.$on('youtube.player.paused', function ($event, player) {
+        $scope.state = $scope.PlaybackState.PAUSED;
         console.log('event: PAUSED');
-//        console.log("Current time:", player.getCurrentTime());
-	});
+    });
 
     $scope.$on('youtube.player.buffering', function ($event, player) {
         $scope.state = $scope.PlaybackState.PAUSED;
         console.log('event: BUFFERING');
     });
 
-	$scope.$on('youtube.player.ended', function ($event, player) {
-		$scope.state = $scope.PlaybackState.STOPPED;
-        console.log('event: ENDED');		
-	});
+    $scope.$on('youtube.player.ended', function ($event, player) {
+        $scope.state = $scope.PlaybackState.STOPPED;
+        console.log('event: ENDED');        
+    });
 
-	$scope.$on('youtube.player.playing', function ($event, player) {
-		$scope.state = $scope.PlaybackState.PLAYING;
+    $scope.$on('youtube.player.playing', function ($event, player) {
+        $scope.state = $scope.PlaybackState.PLAYING;
         $scope.time = Math.floor(player.getCurrentTime() * 1000);
         console.log('event: PLAYING');
-	});
+    });
 
-	$scope.uploadFile = function(files) {
-		var fd = new FormData();
-		//Take the first selected file
-		fd.append("file", files[0]);
+    $scope.uploadFile = function (files) {
+        var srtFile = files[0];
+        var fd = new FormData();
+        fd.append('file', srtFile);
 
-		if (!files[0] || files[0] === $scope.currentSrt)
+        if (!srtFile || srtFile === $scope.currentSrt) {
             return;
+        }
 
-        $scope.currentSrt = files[0];
+        $scope.currentSrt = srtFile;
 
-		$http.post('/sub/upload', fd, {
-			withCredentials: true,
-			headers: {'Content-Type': undefined },
-			transformRequest: angular.identity
-		}).success(function (data) {
-			$scope.subs = data;
-		}).error(function (data) {
+        $http.post('/sub/upload', fd, {
+            withCredentials: true,
+            headers: {'Content-Type': undefined},
+            transformRequest: angular.identity
+        }).success(function (data) {
+            $scope.subs = data;
+        }).error(function (data) {
             if (!$scope.hasAlreadyThrownErrorMessageInUsersFace) {
                 window.alert('Error mate!');
                 $scope.hasAlreadyThrownErrorMessageInUsersFace = true;
             }
-			console.log(data);
-		});
-	};
+            console.log(data);
+        });
+    };
 
-	$scope.isDisplay = function (line) {
-		if ($scope.state == $scope.PlaybackState.STOPPED) return false;
+    $scope.isSubtitleLineDisplayed = function (line) {
+        if ($scope.state === $scope.PlaybackState.STOPPED) {
+            return false;
+        }
+        
         var adjustedTime = $scope.time + $scope.timeOffset;
-		return adjustedTime >= line.startTime &&
+        return adjustedTime >= line.startTime &&
                adjustedTime <= line.endTime;
-	};
-	
-	$scope.isValidId = function () {
-		if (!$scope.player || $scope.player == 'undefined' || $scope.videoId == null || $scope.videoId == '') return true;
-		
-		return $scope.player.getDuration() != 0;
-	}
+    };
+
+    $scope.isValidId = function () {
+        if (!$scope.player || $scope.player === undefined ||
+            $scope.videoId === null || $scope.videoId === '') {
+            return true;
+        }
+
+        return $scope.player.getDuration() !== 0; // TODO: Fix $scope.player.
+    };
 });
