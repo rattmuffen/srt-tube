568178e101b80b66b47b338f997ed74cb6848015
hagward <anders.hagward@gmail.com>
2014-12-09 15:36:47 +0100

    Support for zip files; first step towards #5

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var methodOverride = require('method-override');
 var formidable = require('formidable');
 var fs = require('fs');
 var parser = require('subtitles-parser');
+var admZip = require('adm-zip');
 var app = express();
 
 var port = process.env.PORT || 7845;
@@ -17,11 +18,52 @@ app.use(morgan('dev'));
 
 var router = express.Router();
 
+// Returns the extension, i.e. the text succeeding the last '.', of a given
+// filename in lowercase, or null if no extension is present.
+function getExtension(filename) {
+    var tokenized = filename.split('.');
+
+    if (tokenized.length < 2) {
+        return null;
+    }
+
+    return tokenized[tokenized.length - 1].toLowerCase();
+}
+
+// Unzips a zip file and reads and returns the data of the first file having an
+// 'srt' extension as a utf8 string, or null if no srt file could be
+// found.
+function extractSrtFromZip(filename) {
+    var zip = new admZip(filename);
+    var zipEntries = zip.getEntries();
+
+    for (var i = 0; i < zipEntries.length; i++) {
+        var fileExt = getExtension(zipEntries[i].name);
+        if (fileExt === 'srt') {
+            return zipEntries[i].getData().toString('utf-8');
+        }
+    }
+
+    return null;
+}
+
 router.post('/upload', function (req, res, next) {
     var form = new formidable.IncomingForm();
     form.parse(req, function (err, fields, files) {
-        var f = files.file.path;
-        var data = fs.readFileSync(f, 'utf-8');
+        var data = null;
+        var fileExt = getExtension(files.file.name);
+
+        if (fileExt === 'srt') {
+            data = fs.readFileSync(files.file.path, 'utf-8');
+        } else if (fileExt === 'zip') {
+            data = extractSrtFromZip(files.file.path);
+        }
+
+        if (data === null) {
+            console.log('Error reading file');
+            return;
+        }
+
         var subs = parser.fromSrt(data, true);
         res.send(subs);
     });        
